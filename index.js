f51690e155bb827e7939b332c12b12c7ab685f2b
songyu <1402975007@qq.com>
2023-07-23 16:53:02 +0800

    fix: 首次提交

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ const removeDuplicates = require("./src/Array/removeDuplicates.js");
 const concat = require("./src/Array/concat.js");
 // 深拷贝
 const deepClone = require("./src/Object/deepClone.js");
+// 检测数据类型
+const typeJudgment = require("./src/Object/typeJudgment.js");
 // 向外暴露
 module.exports = {
   ...removeDuplicates,
   ...deepClone,
   ...concat,
+  ...typeJudgment,
 };
